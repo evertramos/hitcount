d163dfacd484b3fd1ac718f4fd75b159ea6a9b0c
Evert Ramos - Nix <evert.ramos@gmail.com>
2016-03-17 07:42:52 -0400

    Revert "backup"
    This reverts commit 8e23a7b7dcb1bf2ba6906186aaf1c94ef5a77473.

--- public/index.php
+++ public/index.php
@@ -41,7 +41,6 @@ if ( $origem == '' ) {
     echo '<th>qtd.</th>';
     echo '<th>criado</th>';
     echo '<th>ultimo</th>';
-    echo '<th>m&eacute;dia mensal</th>';
     echo '</tr>';
 
     foreach ($all as $line) {
@@ -96,4 +95,3 @@ $add = $db->addHit($origem, $destino, $count);
 
 header('Location:http://' . $destino);
 die();
-
